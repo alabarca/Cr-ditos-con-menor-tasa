d2303a7644a36df99d3305d3bdf1534bfceb9bf3
alabarca <32781925+alabarca@users.noreply.github.com>
2017-10-16 16:43:55 -0400

    Add files via upload

---
+++ Nuevos clientes/Resultado.R
@@ -0,0 +1,7 @@
+#Ejemplo rutas
+# ruta1: "C:/Users/alabarca/Desktop"
+# ruta2: "Semana 17_10_2016 al 21_10_2016.xlsx"
+#fecha1: "2016-01-06"
+#fecha2: "2016-01-13"
+
+new("C:/Users/cgomez/Desktop","Semana 30_09_2017 al 06_10_2017.xlsx","2017-09-30","2017-10-06")
